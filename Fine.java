baa771c7f2c674704525d1b9fd8d12df17a93b49
rukhsarvora <87043406+rukhsarvora@users.noreply.github.com>
2021-07-29 16:42:17 -0500

    Update Fine.java

--- Fine.java
+++ Fine.java
@@ -1,7 +1,11 @@
-public class Fine {
-  private Date creationDate;
-  private double bookItemBarcode;
-  private String memberId;
-
-  public static void collectFine(String memberId, long days) {}
-}
+private void checkForFine(String bookItemBarcode) {
+    BookLending bookLending = BookLending.fetchLendingDetails(bookItemBarcode);
+    Date dueDate = bookLending.getDueDate();
+    Date today = new Date();
+    // check if the book has been returned within the due date
+    if (today.compareTo(dueDate) > 0) {
+      long diff = todayDate.getTime() - dueDate.getTime();
+      long diffDays = diff / (24 * 60 * 60 * 1000);
+      Fine.collectFine(memberId, diffDays);
+    }
+  }
