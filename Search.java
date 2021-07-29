c15e2d64246a231393dc132dbd64661ab33014b3
rukhsarvora <87043406+rukhsarvora@users.noreply.github.com>
2021-07-29 16:39:02 -0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,32 +1,33 @@
-public class Catalog {
-  private HashMap<String, List<Book>> bookTitles;
-  private HashMap<String, List<Book>> bookAuthors;
-  private HashMap<String, List<Book>> bookSubjects;
-  private HashMap<String, List<Book>> bookPublicationDates;
-
-}
-
+//public class Catalog {
+  //private HashMap<String, List<Item>> ItemTitles;
+  //private HashMap<String, List<Item>> ItemAuthors;
+ // private HashMap<String, List<Book>> BookSubjects;
+ // private HashMap<String, List<Item>> ItemPublicationDates;
+  //private HashMap<String, List<Book>> BookISBN;
+//}
 public interface Search {
-  public List<Book> searchByTitle(String title);
-  public List<Book> searchByAuthor(String author);
+  public List<Item> searchByTitle(String title);
+  public List<Item> searchByAuthor(String author);
   public List<Book> searchBySubject(String subject);
-  public List<Book> searchByPubDate(Date publishDate);
+  public List<Item> searchByPubDate(Date publishDate);
+  public List<Book> searchByISBN(String ISBN);
 }
 
 public class Catalog implements Search {
-  private HashMap<String, List<Book>> bookTitles;
-  private HashMap<String, List<Book>> bookAuthors;
+  private HashMap<String, List<Item>> bookTitles;
+  private HashMap<String, List<Item>> bookAuthors;
   private HashMap<String, List<Book>> bookSubjects;
-  private HashMap<String, List<Book>> bookPublicationDates;
-
-  public List<Book> searchByTitle(String query) {
+  private HashMap<String, List<Item>> bookPublicationDates;
+  private HashMap<String, List<Book>> bookISBN;
+  
+  public List<Item> searchByTitle(String query) {
     // return all books containing the string query in their title.
-    return bookTitles.get(query);
+    return ItemTitles.get(query);
   }
 
-  public List<Book> searchByAuthor(String query) {
+  public List<Item> searchByAuthor(String query) {
     // return all books containing the string query in their author's name.
-    return bookAuthors.get(query);
+    return ItemAuthors.get(query);
   }
   
   public List<Book> searchBySubjects(String query) {
@@ -34,8 +35,13 @@ public class Catalog implements Search {
     return bookSubjects.get(query);
   }
   
-  public List<Book> searchByPublicationDates(String query) {
+  public List<Item> searchByPublicationDates(String query) {
     // return all books containing the string query in their Publication Dates.
-    return bookPublicationDates.get(query);
+    return ItemPublicationDates.get(query);
+  }
+  
+  public List<Book> searchByISBN(String query) {
+    // return all books containing the string query in their Publication Dates.
+    return bookISBN.get(query);
   }
 }
