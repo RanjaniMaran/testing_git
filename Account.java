791df2a802cc76547d952705f2c63ef8cd6196ee
rukhsarvora <87043406+rukhsarvora@users.noreply.github.com>
2021-07-30 10:22:43 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,25 +1,106 @@
-// User classes can extend from this class and have user information
-
 public abstract class Account {
   private String id;
   private String password;
   private AccountStatus status;
   private Person person;
-
+  public String itemid;
+  public String itemName;
+  public String itemCategory;
+  public String Username;
+  public String libraryCardNumber;
+  public Date issueDate;
+  public Date returnDate;
+  public String status;
+  public int fine;
+  
   public boolean resetPassword();
+
+
+public void accountKeeping(String libraryCardNumber) {
+	switch(status) {
+	case issued: 
+		FileWriter csvWriter = new FileWriter(libraryCardNumber+".csv");
+		csvWriter.append(ItemId);
+		csvWriter.append(",");
+		csvWriter.append(ItemName);
+		csvWriter.append(",");
+		csvWriter.append(ItemCategory);
+		csvWriter.append(",");
+		csvWriter.append(Username);
+		csvWriter.append(",");
+		csvWriter.append(LibraryCardNumber);
+		csvWriter.append(",");
+		csvWriter.append(issueDate);
+		csvWriter.append(",");
+		csvWriter.append(returnDate);
+		csvWriter.append(",");
+		csvWriter.append(status);
+		csvWriter.append(",");
+		csvWriter.append(fine);
+		csvWriter.append("\n");
+
+	case returned:
+		FileWriter csvWriter = new FileWriter(libraryCardNumber+".csv");
+		csvWriter.append(ItemId);
+		csvWriter.append(",");
+		csvWriter.append(ItemName);
+		csvWriter.append(",");
+		csvWriter.append(ItemCategory);
+		csvWriter.append(",");
+		csvWriter.append(Username);
+		csvWriter.append(",");
+		csvWriter.append(LibraryCardNumber);
+		csvWriter.append(",");
+		csvWriter.append(issueDate);
+		csvWriter.append(",");
+		csvWriter.append(returnDate);
+		csvWriter.append(",");
+		csvWriter.append(status);
+		csvWriter.append(",");
+		csvWriter.append(fine);
+		csvWriter.append("\n");
+		
+	case renewed:
+		FileWriter csvWriter = new FileWriter(libraryCardNumber+".csv");
+		csvWriter.append(ItemId);
+		csvWriter.append(",");
+		csvWriter.append(ItemName);
+		csvWriter.append(",");
+		csvWriter.append(ItemCategory);
+		csvWriter.append(",");
+		csvWriter.append(Username);
+		csvWriter.append(",");
+		csvWriter.append(LibraryCardNumber);
+		csvWriter.append(",");
+		csvWriter.append(issueDate);
+		csvWriter.append(",");
+		csvWriter.append(returnDate);
+		csvWriter.append(",");
+		csvWriter.append(status);
+		csvWriter.append(",");
+		csvWriter.append(fine);
+		csvWriter.append("\n");
+		
+	case viewed:
+		FileWriter csvWriter = new FileWriter(libraryCardNumber+".csv");
+		csvWriter.append(ItemId);
+		csvWriter.append(",");
+		csvWriter.append(ItemName);
+		csvWriter.append(",");
+		csvWriter.append(ItemCategory);
+		csvWriter.append(",");
+		csvWriter.append(Username);
+		csvWriter.append(",");
+		csvWriter.append(LibraryCardNumber);
+		csvWriter.append(",");
+		csvWriter.append(issueDate);
+		csvWriter.append(",");
+		csvWriter.append(returnDate);
+		csvWriter.append(",");
+		csvWriter.append(status);
+		csvWriter.append(",");
+		csvWriter.append(fine);
+		csvWriter.append("\n");
+	}
 }
-
-//I found this example online only for reference
-//Please check what we can change and do it our way
-
-private void checkForFine(String bookItemBarcode) {
-    BookLending bookLending = BookLending.fetchLendingDetails(bookItemBarcode);
-    Date dueDate = bookLending.getDueDate();
-    Date today = new Date();
-    // check if the book has been returned within the due date
-    if (today.compareTo(dueDate) > 0) {
-      long diff = todayDate.getTime() - dueDate.getTime();
-      long diffDays = diff / (24 * 60 * 60 * 1000);
-      Fine.collectFine(memberId, diffDays);
-    }
-  }
+}
