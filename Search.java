8fc86be5d1dacd2a765ce6625e4afc463960db2d
rukhsarvora <87043406+rukhsarvora@users.noreply.github.com>
2021-07-31 16:56:13 -0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,43 +1,25 @@
 import java.util.HashMap;
 import java.util.List;
 
-public interface Search {
-  public List<Item> searchByTitle(String title);
-  public List<Item> searchByAuthor(String author);
-  public List<Book> searchByCategory(String category);
-  public List<Item> searchByPubDate(Date publishDate);
-  public List<Book> searchByISBN(int isbn);
+public class SearchItem {
+	SearchItem(){}
+	
+	public String[] searchItem(String title) throws IOException {
+		try 
+		{
+			BufferedReader csvReader = new BufferedReader(new FileReader("Books.csv"));
+			String row;
+			while ((row = csvReader.readLine()) != null) {
+			    String[] data = row.split(",");
+			    System.out.println(data[0]);
+			if (data[1].equals(title)) {
+				return data;
+			}
+			}
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 }
-
-public class Catalog implements Search {
-  private HashMap<String, List<Item>> itemTitles;
-  private HashMap<String, List<Item>> itemAuthors;
-  private HashMap<String, List<Book>> bookCategory;
-  private HashMap<String, List<Item>> itemPublicationDates;
-  private HashMap<String, List<Book>> bookISBN;
-  
-  public List<Item> searchByTitle(String query) {
-    // return all books containing the string query in their title.
-    return itemTitles.get(query);
-  }
-
-  public List<Item> searchByAuthor(String query) {
-    // return all books containing the string query in their author's name.
-    return itemAuthors.get(query);
-  }
-  
-  public List<Book> searchByCategory(String query) {
-    // return all books containing the string query in their category.
-    return bookCategory.get(query);
-  }
-  
-  public List<Item> searchByPublicationDates(String query) {
-    // return all books containing the string query in their Publication Dates.
-    return itemPublicationDates.get(query);
-  }
-  
-  public List<Book> searchByISBN(String query) {
-    // return all books containing the string query in their ISBN.
-    return bookISBN.get(query);
-  }
 }
