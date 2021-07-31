512044d5384e1362333e785390193b9a504efd26
manideepika541 <87250655+manideepika541@users.noreply.github.com>
2021-07-31 14:09:06 -0500

    Updated transactio with using excel

--- Transaction.java
+++ Transaction.java
@@ -1,188 +1,148 @@
 package librarysystem.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Transaction{
 
-	int tempUserId = -5; // some garbage
-	int tempItemId = -5; // some garbage
-	boolean child = false,other = false ;
-
-
-	int validUser(int userId){
-	
-		for(int i=0;i<DAO.childrenUsers.length;i++){
-			if(DAO.childrenUsers[i].id == userId){
-				child = true ; 
-				return i;
-			}
-		}
-
-		for(int i=0;i<DAO.otherUsers.length;i++)
-			if(DAO.otherUsers[i].id == userId){
-				other = true ; return i;
-			}
-		return -1;
-	}
-
-	int validItem(int itemId){
-
-		for(int i=0;i<DAO.hardMedias.length;i++)
-			if(DAO.hardMedias[i].id == itemId)
-				return i;
-
-		return -1;
-	}
-	
-	ChildrenUser getChildUser(int libraryCardNumber)
+	boolean isChildUser(String libraryCardNumber)
 	{
-		ChildrenUser childrenUser = null;
-		for(int i=0;i<DAO.childrenUsers.length;i++){
-			if(DAO.childrenUsers[i].library_card_number == libraryCardNumber){
-				childrenUser = DAO.childrenUsers[i];
+		BufferedReader csvReader = null;
+		try {
+			csvReader = new BufferedReader(new FileReader("Userdetails.csv"));
+			String row;
+			while ((row = csvReader.readLine()) != null) {
+			    String[] data = row.split(",");
+			    int age = Integer.parseInt(data[5]);
+			    String libraryCardNumberOfUser = data[6];
+			    if (libraryCardNumber.equals(libraryCardNumberOfUser)) {
+			    	csvReader.close();
+			    	if(age <= 12)
+			    	{
+			    		return true;
+			    	}
+				}
+			   
 			}
+			 
 		}
-		
-		return childrenUser;
+		catch (IOException x) {
+			System.err.println(x);
+		}
+		csvReader.close();
+		return false;
 	}
 	
-	int getNumberOfItemCheckedOutByUser(int libraryCardNumber)
+	boolean isItemAudio(String itemId)
 	{
-		int count = 0;
-		for(int i=0;i<DAO.issuedBooks.length;i++){
-			if(DAO.issuedBooks[i].library_card_number == libraryCardNumber){
-				count++;
-			}
+		if(itemId.startsWith("A"))
+		{
+			return true;
 		}
-		
+		else
+		{
+			return false;
+		}
+	}
+	
+	boolean isItemVideo(String itemId)
+	{
+		if(itemId.startsWith("V"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	boolean isBestSeller(String itemId)
+	{
+		if(itemId.startsWith("BS"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	boolean isReferenceBooksOrMagazine(String itemId)
+	{
+		if(itemId.startsWith("R"))
+		{
+			return true;
+		}
+		else if(itemId.startsWith("M"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public int getNumberOfItemCheckedOutByUser(String libraryCardNumber)
+	{
+		int count;
+		BufferedReader csvReader = null;
+		try {
+			csvReader = new BufferedReader(new FileReader(libraryCardNumber+".csv"));
+			String row;
+			while ((row = csvReader.readLine()) != null) {
+			    String[] data = row.split(",");
+			    String status = data[7];
+			    if (status.equals("issued")) {
+			    	count++;
+				}
+			   
+			}
+			 
+		}
+		catch (IOException x) {
+			System.err.println(x);
+		}
+		csvReader.close();
 		return count;
-	}
-	
-	boolean isItemAudioOrVideo(int itemId)
-	{
-		for(int i = 0 ; i<DAO.audioItems.length; i++)
-		{
-			if(DAO.audioItems[i].id == itemId)
-			{
-				return true;
-			}
-		}
 		
-		for(int i = 0 ; i<DAO.videoItems.length; i++)
-		{
-			if(DAO.videoItems[i].id == itemId)
-			{
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	boolean isBestSeller(int itemId)
-	{
-		for(int i = 0 ; i<DAO.bestSellerItems.length; i++)
-		{
-			if(DAO.bestSellerItems[i].id == itemId)
-			{
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	boolean isReferenceBooksOrMagazine(int itemId)
-	{
-		for(int i = 0 ; i<DAO.refernceBooks.length; i++)
-		{
-			if(DAO.refernceBooks[i].id == itemId)
-			{
-				return true;
-			}
-		}
-		
-		for(int i = 0 ; i<DAO.magazines.length; i++)
-		{
-			if(DAO.magazines[i].id == itemId)
-			{
-				return true;
-			}
-		}
-		
-		return false;
 	}
 
 
-	public String checkoutItem(int libraryCardNumber,int itemId){
-		tempUserId = validUser(libraryCardNumber);
-		if( tempUserId != -1)
+	public String checkoutItem(String libraryCardNumber,String itemId){
+			
+		if(isReferenceBooksOrMagazine(itemId))
 		{
-			tempItemId = validItem(itemId);
-			if( tempItemId != -1){
-				
-				if(isReferenceBooksOrMagazine(itemId))
-				{
-					return "Any user cannot checkout reference books or magazines";
-				}
-				
-				ChildrenUser childrenUser = getChildUser(libraryCardNumber);
-				
-				if(childrenUser)
-				{
-					if(getNumberOfItemCheckedOutByUser(libraryCardNumber) >= 5)
-					{
-						return "Children user can only checkout five items at a time";
-					}
-				}
-				
-				boolean isItemAudioOrVideo = isItemAudioOrVideo(itemId);
-				
-				if(isItemAudioOrVideo || isBestSeller(itemId))
-				{
-					//Write logic to checkout the item for only 2 weeks
-				}
-				else
-				{
-					//Write logic to checkout the item for 3 weeks
-				}
+			return "Any user cannot checkout reference books or magazines";
+		}
+		
+		if(isChildUser(libraryCardNumber))
+		{
+			if(getNumberOfItemCheckedOutByUser(libraryCardNumber) >= 5)
+			{
+				return "Children user can only checkout 5 items at a time";
 			}
-			else
-				return ("\nOops ! Item not found. Please Enter valid Item ID.");
 		}
 		else
-			return ("\nOops ! User not recognized. Please enter valid User ID.");
-
-	}
-
-	public String returnItem(int libraryCardNumber,int itemId){
-
-		// Let's hope that user can return book so no need to check balance 
-
-		if((tempUserId = validUser(libraryCardNumber)) != -1){
-		
-			if((tempItemId = validItem(itemId)) != -1){
-
-				for(int i=0;i<DAO.issuedBooks.length;i++){
-					if(DAO.issuedBooks[i].library_card_number == libraryCardNumber && DAO.issuedBooks[i].id == itemId){
-						//Write code to remove it from the issued books
-						//Increase quantity
-					}
-				}
-				
-				/*
-				 * DAO.hardMedias[tempItemId].quantity++;
-				 * DAO.hardMedias[tempItemId].availibility = true;
-				 */
-
-				return ("\nBook " + DAO.hardMedias[tempItemId].title + " Returned SuccessFully.");
+		{
+			if(getNumberOfItemCheckedOutByUser(libraryCardNumber) >= 12)
+			{
+				return "User can only checkout 12 items at a time";
 			}
-			else
-				return ("\nOops ! Book not found. Please Enter valid Book ID.");
+		}
+		
+		if(isItemAudio(itemId) || isItemVideo(itemId) || isBestSeller(itemId))
+		{
+			//Write logic to checkout the item for only 2 weeks
 		}
 		else
-			return ("\nOops ! User not recognized. Please enter valid User ID.");
+		{
+			//Write logic to checkout the item for 3 weeks
+		}
 
 	}
-
-	
 }
